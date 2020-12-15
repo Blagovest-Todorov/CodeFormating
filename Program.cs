86d9cba4cd8422d3fabb1b496a799163ee5623bb
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-12-15 15:39:59 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace CodeFormating
+{
+    using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Code Formating
+            // CodeFormating Methods
+            // FormatingTypes, 
+            // Common mistakes
+            // Alignments, 
+            // AutomatedTools
+            // the idea of code formating is :
+            // the Code Formating :
+            // idea is to :  improve readability 
+            // to improve maintaintability
+            // the good formating do not add some speed and memory usage of this 
+            // use empty linesd between the methods !
+            // in a class the methods must be indentated with a tab 
+            // add curly barackets afet if(), Else{}, for{}. while{}, do{}while();
+            // after if, for, while .. put an one empty line 
+            // Style Cop Extension in VS is used for better foramtong of the code for better rradability !
+
+        }
+    }
+}
